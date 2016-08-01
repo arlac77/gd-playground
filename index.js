acafcc3daac0406b74287e8a8de85b6fd46d429f
Markus Felten <Markus.Felten@gmx.de>
2016-08-01 19:28:30 +0200

    wip

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 /* jslint node: true, esnext: true */
 'use strict';
 
-
 const levelup = require('levelup'),
   levelws = require('level-ws');
 
 
-const NUMBER_OF_ENTRIES = 1000000;
+const NUMBER_OF_ENTRIES = 100000;
 const REPORT_NUMBER = 10000;
+const VERSIONS_NUMBER = 2;
 
 const mydb = levelup('./my.levelup');
+
+const keyLength = 4 + 4;
+const chunkSize = 1000;
 
 const w = false;
 
@@ -24,43 +27,53 @@ if (w) {
   ws.on('error', err => console.error('Oh my!', err));
   ws.on('close', () => console.log('Stream closed'));
 
-  const version = 6;
-  const chunkSize = 1000;
-  const keyLength = 4 + 4;
-  let b0 = new Buffer(keyLength * chunkSize);
-  let start = 0;
+  for (let version = 0; version < VERSIONS_NUMBER; version++) {
+    //const version = 6;
+    let b0 = new Buffer(keyLength * chunkSize);
+    let start = 0;
 
-  for (let i = 0; i < NUMBER_OF_ENTRIES; i++) {
-    const b = b0.slice(start, start + keyLength);
-    start += keyLength;
+    for (let i = 0; i < NUMBER_OF_ENTRIES; i++) {
+      const b = b0.slice(start, start + keyLength);
+      start += keyLength;
 
-    if (start >= chunkSize) {
-      start = 0;
-      b0 = new Buffer(keyLength * chunkSize);
+      if (start >= chunkSize) {
+        start = 0;
+        b0 = new Buffer(keyLength * chunkSize);
+      }
+
+      b.writeInt32BE(i);
+      b.writeInt32BE(version, 1);
+
+      if (i % REPORT_NUMBER === 0) {
+        console.log(`${version} ${i}`);
+      }
+
+      ws.write({
+        key: b,
+        value: `Name ${i} ${version}`
+      });
     }
-
-    b.writeInt32BE(i);
-    b.writeInt32BE(version, 1);
-
-    if (i % REPORT_NUMBER === 0) {
-      console.log(i);
-    }
-
-    ws.write({
-      key: b,
-      value: `Name ${i} ${version}`
-    });
   }
 
   ws.end();
   console.log('end called...');
 } else {
-  mydb.createReadStream({
-      keyEncoding: 'binary',
-      valueEncoding: 'utf8'
-    })
-    .on('data', data => console.log(data.key.readInt32BE(0), data.key.readInt32BE(1), data.value))
-    .on('error', err => console.error('Oh my!', err))
-    .on('close', () => console.log('Stream closed'))
-    .on('end', () => console.log('Stream closed'));
+  let buf = new Buffer(keyLength);
+  buf.writeInt32BE(100000, 0);
+  buf.writeInt32BE(0, 0);
+
+  mydb.get(buf, (error, data) => {
+    console.log(data);
+  });
+
+  /*
+    mydb.createReadStream({
+        keyEncoding: 'binary',
+        valueEncoding: 'utf8'
+      })
+      .on('data', data => console.log(data.key.readInt32BE(0), data.key.readInt32BE(1), data.value))
+      .on('error', err => console.error('Oh my!', err))
+      .on('close', () => console.log('Stream closed'))
+      .on('end', () => console.log('Stream closed'));
+      */
 }
